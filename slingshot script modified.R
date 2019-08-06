033a2c3c19c810bd5bd5e965d99400dc4de0d1ad
GTI77 <45839161+gxe778@users.noreply.github.com>
2019-08-06 11:13:12 -0700

    Add files via upload

---
+++ slingshot script modified.R
@@ -0,0 +1,47 @@
+#Slingshot script
+
+#https://github.com/kstreet13/slingshot
+
+BiocManager::install("kstreet13/slingshot")
+
+library(slingshot)
+
+#Load Data
+TI_Data_Yan_et_al <- read_excel("TI Data Yan et al.xlsx")
+
+
+TI_Data_Yan_et_al<-as.data.frame(TI_Data_Yan_et_al, stringsAsFactors = FALSE)
+
+
+TID<-TI_Data_Yan_et_al
+
+
+rownames(TID)<-TI_Data_Yan_et_al[,1]
+
+
+TID<-TID[,-c(1:2)]
+
+
+#save data in excel and R
+save(TID, file="TID.xlsx")
+
+save(TID,file="TID.Rda")
+
+
+#Create SingleCell Object
+counts<-as.matrix(TID)
+
+v<-log2(counts+1)
+
+
+# create a SingleCellExperiment object sce
+sce <- SingleCellExperiment::SingleCellExperiment(assay = list(counts = counts, logcounts=v))
+
+
+#Dimension Reduction
+
+PCA <- prcomp(t(log1p(v)))
+
+
+#Slingshot
+sim <- slingshot(v,  reducedDim='PCA')
