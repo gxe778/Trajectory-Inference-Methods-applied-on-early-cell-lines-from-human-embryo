b5e6cc8dea159d09ce6602ba62055612fb475c73
GTI77 <45839161+gxe778@users.noreply.github.com>
2019-08-07 10:29:27 -0700

    Add files via upload

---
+++ phenopath modified script.R
@@ -0,0 +1,49 @@
+#phenopath script
+
+#https://github.com/kieranrcampbell/phenopath
+
+devtools::install_github("kieranrcampbell/phenopath", build_vignettes = TRUE)
+
+library(phenopath)
+
+#Load Data
+
+TI_Data_Yan_et_al <- read_excel("TI Data Yan et al.xlsx")
+
+
+TI_Data_Yan_et_al<-as.data.frame(TI_Data_Yan_et_al, stringsAsFactors = FALSE)
+
+
+TID<-TI_Data_Yan_et_al
+
+
+rownames(TID)<-TI_Data_Yan_et_al[,1]
+
+
+TID<-TID[,-c(1:2)]
+
+
+#save data in excel and R
+save(TID, file="TID.xlsx")
+
+save(TID,file="TID.Rda")
+
+
+#Create SingleCell Object
+counts<-as.matrix(TID)
+
+v<-log2(counts+1)
+
+sce <- SingleCellExperiment(assay = list(counts = counts, logcounts=v))
+
+#Phenopath pre-processing
+x <- 2 * (v== "v") - 1
+
+scale_vec <- function(x) (x - mean(x)) / sd(x)
+pc1 <- prcomp(t(x))
+pc1 <- scale_vec(pc1)
+time_numeric <- as.numeric(gsub("h", "", sce$time))
+pc1 <- pc1 * sign(cor(pc1, time_numeric))
+
+#Phenopath
+fit <- phenopath(v, x)
