0262a8773aa0884bcf19dd8e0d9f066247ccd071
GTI77 <45839161+gxe778@users.noreply.github.com>
2019-08-10 11:33:43 -0700

    Update TSCAN modified.R

--- TSCAN modified.R
+++ TSCAN modified.R
@@ -20,19 +20,20 @@ TID<-TID[,-c(1:2)]
 
 
 #save data in excel and R
-save(TID, file="TID.xlsx")
-
 save(TID,file="TID.Rda")
 
+write.xlsx2(TID, file="TID.xlsx")
 
-#Create SingleCell Object
+
+# create a SingleCellExperiment object sce
+
 counts<-as.matrix(TID)
 
 v<-log2(counts+1)
 
 
-# create a SingleCellExperiment object sce
 sce <- SingleCellExperiment(assay = list(counts = counts, logcounts=v))
+
 
 #load/process data
 data<-v
