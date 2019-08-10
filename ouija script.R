c79304c469cec2d56b46752c8119e88d56b9d6b6
GTI77 <45839161+gxe778@users.noreply.github.com>
2019-08-10 11:37:27 -0700

    Update ouija script.R

--- ouija script.R
+++ ouija script.R
@@ -20,17 +20,18 @@ rownames(TID)<-TI_Data_Yan_et_al[,1]
 TID<-TID[,-c(1:2)]
 
 #save data in excel and R
-save(TID, file="TID.xlsx")
-
 save(TID,file="TID.Rda")
 
-#Create SingleCell Object
+write.xlsx2(TID, file="TID.xlsx")
+
+
+# create a SingleCellExperiment object sce
 
 counts<-as.matrix(TID)
 
 v<-log2(counts+1)
 
-# create a SingleCellExperiment object sce
+
 sce <- SingleCellExperiment(assay = list(counts = counts, logcounts=v))
 
 #Ouija
