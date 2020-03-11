5b3ec68737fc7319a278ab875f41579fe59059b9
GTI77 <45839161+gxe778@users.noreply.github.com>
2020-03-11 11:40:21 -0700

    Update tSpace.R

--- tSpace.R
+++ tSpace.R
@@ -1,20 +1,13 @@
-                # tSpace
-
-                # https://github.com/hylasD/tSpace
+                 #tSpace
 
 
 
-
-
-# Install/Load tSpace
-
-
-#devtools::install_github('hylasD/tSpace', build = TRUE, build_opts = c("--no-resave-data", "--no-manual"), force = T)
-
+devtools::install_github('hylasD/tSpace', build = TRUE, build_opts = c("--no-resave-data", "--no-manual"), force = T)
 
 library(tSpace)
 
-#Load Data
+
+#load data
 
 TI_Data_Yan_et_al <- read_excel("TI_Yan.xlsx")
 
@@ -44,14 +37,14 @@ TID<-TID[,-c(1:2)]
 
 
 
-# save data in excel and R:
+#save data in excel and R:
 
 
-# save(TID,file="TID.Rda")
+   save(TID, file="TID.Rda")
 
 
 
-# write.xlsx2(TID, file="TID.xlsx")
+    write.xlsx2(TID, file="TID.xlsx")
 
 
 
@@ -63,36 +56,36 @@ counts<-as.matrix(TID)
 v<-log2(counts+1)
 
 
-# save: excel and R:
+#save: excel and R:
 
 
-# save(v,file="v.Rda")
+   save(v,file="v.Rda")
 
 
-# write.xlsx2(v, file="v.xlsx")
+   write.xlsx2(v, file="v.xlsx")
 
 
 
-# create a SingleCellExperiment object sce
+#create a SingleCellExperiment object sce
 
 
 sce <- SingleCellExperiment(assay = list(counts = counts, logcounts=v))
 
 
 
-# run tSpace 
+#tSpace 
 
 
 ts <- tSpace(df = v, K = 20, L = 15, D = 'pearson_correlation', graph = 5, trajectories = 200, wp = 15, dr = 'pca', core_no = 2)
 
 
-# tSpace: 1000 trajectories (i.e. cell # < 10000) 
+#tSpace: 1000 trajectories (i.e. cell # < 10000) 
 
 
 ts <- tSpace(df = v, K = 20, L = 15, D = 'pearson_correlation', graph = 5, trajectories = 1000, wp = 15, dr = 'pca', core_no = 2)
 
 
-# tSpace: UMAP
+#tSpace: UMAP
 
 
 ts <- tSpace(df = v, K = 20, L = 15, D = 'pearson_correlation', graph = 5, trajectories = 1000, wp = 15, dr = 'umap', core_no = 2)
